ebf47fb08db1450079839b334ccf39d1efae641b
amainooti <amainooti@gmail.com>
2023-03-26 18:16:49 +0100

    Made few changes to the folder structure, added the templating engine

--- runAm/server.js
+++ runAm/server.js
@@ -2,22 +2,26 @@
 // boiler plate code has no real use
 
 const express = require('express');
+const path = require('path');
 const app = express();
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+
+// displaying static files
+
+app.use(express.static(path.join(__dirname, 'public')));
 // use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('index');
 });
 
 // about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
